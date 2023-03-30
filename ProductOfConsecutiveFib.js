44ac5a60c1a284af3df6a1bd5140af23da732333
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-03-30 17:38:34 -0600

    added chai to test challenges

--- ProductOfConsecutiveFib.js
+++ ProductOfConsecutiveFib.js
@@ -1,3 +1,5 @@
+const { sameOrderedMembers } = require('chai').assert;
+
 /*
 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
 F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1
@@ -17,4 +19,14 @@ function productFib(prod){
   return [55, 89, true];
 }
 
-console.log(`productFib(4895) returns [55, 89, true]? ${productFib(4895)}`);
+try {
+  sameOrderedMembers(productFib(4895), [55, 89, true], 'Test 1');
+  sameOrderedMembers(productFib(5895), [89, 144, false], 'Test 2');
+  sameOrderedMembers(productFib(74049690), [6765, 10946, true], 'Test 3');
+  sameOrderedMembers(productFib(84049690), [10946, 17711, false], 'Test 4');
+  sameOrderedMembers(productFib(193864606), [10946, 17711, true], 'Test 5');
+  sameOrderedMembers(productFib(447577), [610, 987, false], 'Test 6');
+  sameOrderedMembers(productFib(602070), [610, 987, true], 'Test 7');
+} catch (error) {
+  console.log("Test failure ==>", error);  
+}

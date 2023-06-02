fb9e7c45f417c9005028f4b52907c4f150b3cb83
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-02 08:54:15 -0600

    added coinChange challenge

---
+++ coinChange.js
@@ -0,0 +1,6 @@
+// Input - a number of coins, ex. $10, $5, $2, $1. A number to make change for, ex. $17
+// Output - Least amount of coins to make change
+
+function coinChange(coins, num) {
+  //TODO: Implement coin change function
+}

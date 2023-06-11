eb804d74a6b1a177aa130b38d7e97958c459f164
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-11 12:17:39 -0600

    added solution & unit test

--- secondLargestSmallest.js
+++ secondLargestSmallest.js
@@ -1,3 +1,5 @@
+const expect = require('chai').expect;
+
 // Find the second largest and second smallest numbers in an unsorted array with duplicate values.
 
 // Input: unsorted array with duplicate values
@@ -6,5 +8,16 @@
 const exampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 55, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 55];
 
 function secondLargestSmallest(arrayNums) {
-    //TODO: Write code here
+    // create container for second largest and second smallest
+    let arr = [];
+    // sort the array acsending
+    arrayNums.sort((a,b) => a - b);
+    // get rid of duplicates
+    const noDupes = [...new Set(arrayNums)];
+    // push the second largest and second smallest
+    arr.push(noDupes[noDupes.length - 2], noDupes[1]);
+    // return the container
+    return arr;
 };
+
+expect(secondLargestSmallest(exampleArray)).to.eql([30, 2]);

2755ff64544cb60e9f861803888e4a7e87f72010
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-14 07:58:46 -0600

    added mostCommonWordSize

---
+++ mostCommonWordSize.js
@@ -0,0 +1,6 @@
+// Input: a string - "The dog jumped over the fence"
+// Output: an integer that represents the most common word size in the provided sentence - 3
+
+function mostCommonWordSize(str) {
+    //TODO: Implement
+}

8e2cb80cdb2aba47bde4434d6ddb1c9ece0e16b6
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-19 17:06:45 -0600

    added pairsArrToSumValue

---
+++ pairsArrToSumValue.js
@@ -0,0 +1,12 @@
+// Find all pairs of integers within a list which sum to a specified value (n).
+// Input: Array of integers
+// Output: Array of pairs of integers
+// Examples:
+    // pairsArrToSumValue([1, 2, 3, 4, 5], 5) => [[1, 4], [2, 3]]
+    // pairsArrToSumValue([1, 2, 3, 4, 5], 10) => []
+    // pairsArrToSumValue([0, 2, 3, 4, 5], 5) => [[0, 5]]
+    // pairsArrToSumValue([1, 2, 3, 4, 5], 4) => [[1, 3]]
+
+function pairsArrToSumValue(arr, sum) {
+    //TODO: Implement
+};

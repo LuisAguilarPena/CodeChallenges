1717cee3181d588eff7c5feb40a4865a0c0842e8
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-23 14:15:21 -0600

    solution and tests

--- validPhoneNumber.js
+++ validPhoneNumber.js
@@ -1,7 +1,21 @@
+const expect = require('chai').expect;
+
 // Write a function to validate phone number in  XXX-XXX-XXXX or (XXX) XXX-XXXX formats?
 // Input: string, ex: 123-456-7890 or (123) 456-7890
 // Output: boolean, ex: true or false, if the input is valid or not,respectively
 
 function validPhoneNumber(phoneNumber) {
-    //TODO: write your code here
+    // create regex for the two formats
+    let phoneUSFormatNoPar = /^\d{3}-\d{3}-\d{4}$/;
+    let phoneUSFormatPar = /^\(\d{3}\) \d{3}-\d{4}$/;
+    // test the input against the two formats
+    if(phoneUSFormatNoPar.test(phoneNumber) || phoneUSFormatPar.test(phoneNumber)) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+expect(validPhoneNumber('123-456-7890')).to.be.true;
+expect(validPhoneNumber('(123) 456-7890')).to.be.true;
+expect(validPhoneNumber('1234567890')).to.be.false;

649b29434cdeb82b15f4a139521c968f759fa127
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-23 14:32:57 -0600

    added firstRepeatedCharacter

---
+++ firstRepeatedCharacter.js
@@ -0,0 +1,7 @@
+// Write a function that takes a string and returns the first repeated character
+// Input: string, ex: "hello"
+// Output: string, ex: "l"
+
+function firstRepeatedCharacter(string) {
+    //TODO: write your code here
+}

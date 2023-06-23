3dc0bd0fc9acc8f06af33827d078082180186b92
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-23 13:27:50 -0600

    added validPhoneNumber

---
+++ validPhoneNumber.js
@@ -0,0 +1,7 @@
+// Write a function to validate phone number in  XXX-XXX-XXXX or (XXX) XXX-XXXX formats?
+// Input: string, ex: 123-456-7890 or (123) 456-7890
+// Output: boolean, ex: true or false, if the input is valid or not,respectively
+
+function validPhoneNumber(phoneNumber) {
+    //TODO: write your code here
+}

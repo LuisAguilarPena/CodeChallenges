334a3c017fbc79b638d7e68c9ea05c7b9d55df9e
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2023-06-25 07:56:13 -0600

    added solution

--- reverseWordOrder.js
+++ reverseWordOrder.js
@@ -1,5 +1,10 @@
 // Reverse the word order in a sentence: I am a student => student a am I
 
 function reverseWordOrder(str) {
-    //TODO: Write - Your - Code
+    // split the string into an array of words
+    // reverse the array
+    // join the array into a string
+    return str.split(' ').reverse().join(' ');
 }
+
+console.log(reverseWordOrder('I am a student')); // student a am I
